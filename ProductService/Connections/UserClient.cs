2cbc27666a6391e1f9fdf7802c89c417f17cf1f6
anasbenmguirida <anasbenmki@gmail.com>
2025-04-29 12:11:08 +0100

    starting the config for a Apigateway

---
+++ ProductService/Connections/UserClient.cs
@@ -0,0 +1,20 @@
+﻿using Service2.DTO;
+
+
+
+public class UserClient
+{
+    private HttpClient _httpClient;
+
+    public UserClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient; 
+    }
+    public async Task<string> GetUserNames()
+    {
+        var response = await _httpClient.GetAsync($"http://localhost:5212/api/person/all");
+        response.EnsureSuccessStatusCode(); 
+        return await response.Content.ReadAsStringAsync();
+        
+    }
+}
